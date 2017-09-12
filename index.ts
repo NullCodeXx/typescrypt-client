1c8d6b0b22eb97cc518225fa30d18fbb2b5cb7db
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-12 11:27:01 +0200

    assign button for increment and decrement

--- index.ts
+++ index.ts
@@ -7,3 +7,18 @@ compteur.increment();//2
 compteur.increment();//3
 
 console.log(compteur.displayCount());
+
+// Assigner la function increment au button
+
+const para = document.querySelector('.para');
+const btnIncrement = document.querySelector('.increment');
+const btnDecrement = document.querySelector('.decrement');
+
+btnIncrement.addEventListener('click', ()=>{
+    compteur.increment();
+    para.textContent = compteur.displayCount().toString();
+})
+btnDecrement.addEventListener('click', ()=>{
+    compteur.decrement();
+    para.textContent = compteur.displayCount().toString();
+})
